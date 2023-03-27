35205b255dbf6c5ac571b94ca044137a40c8b8ac
Federico Airoldi <airoldibusiness@gmail.com>
2023-03-27 23:42:28 +0200

    mail - creo il secondo metodo per cntrollare l'email

--- mail/asset/js/main.js
+++ mail/asset/js/main.js
@@ -11,14 +11,34 @@ let arrayEmail = ['emma.smith@gmail.com', 'paul.taylor@gmail.com', 'liam.lee@gma
 
 
 function check() {
+
     let emailtocheck = document.getElementById('emailtocheck').value;
     
+    //metodo 1
+    // if (arrayEmail.indexOf(emailtocheck) !== -1) {
+    //     document.getElementById('result').innerHTML = 'corretto';
+    // }
+    // else {
+    //     document.getElementById('result').innerHTML = 'sbagliato';
+    // }
 
-    if (arrayEmail.indexOf(emailtocheck) !== -1) {
-        document.getElementById('result').innerHTML = 'corretto';
-    }
-    else {
-        document.getElementById('result').innerHTML = 'sbagliato';
+    //metodo 2
+
+
+    for (let i = 0; i < arrayEmail.length; i++) {
+
+        let arrayEmail = ['emma.smith@gmail.com', 'paul.taylor@gmail.com', 'liam.lee@gmail.com', 'frank.jones@gmail.com', 'steve.evans@gmail.com', 'steve.taylor@gmail.com', 'isabel.klein@gmail.com', 'mary.lee@gmail.com', 'bob.davis@gmail.com', 'mary.irwin@gmail.com', 'isabel.patel@gmail.com', 'liam.carter@gmail.com', 'tina.brown@gmail.com', 'henry.nguyen@gmail.com', 'grace.carter@gmail.com', 'tina.patel@gmail.com', 'isabel.robinson@gmail.com', 'isabel.irwin@gmail.com', 'emma.robinson@gmail.com', 'paul.carter@gmail.com']
+
+        
+        let emailtocheck = document.getElementById('emailtocheck').value;
+
+        if (arrayEmail[i] == emailtocheck) {
+            document.getElementById('result').innerHTML = 'corretto';
+            console.log(arrayEmail[i])
+        } else {
+            document.getElementById('result').innerHTML = 'sbagliato';
+            console.log(arrayEmail[i])
+        }
     }
 
 
