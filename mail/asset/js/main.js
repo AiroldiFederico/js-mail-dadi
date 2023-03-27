afad8dcf79ba077eeeb799412130722b52eb471a
Federico Airoldi <airoldibusiness@gmail.com>
2023-03-27 19:08:43 +0200

    FASE 1 - inizio a impostare gli elementi in html e css

--- mail/asset/js/main.js
+++ mail/asset/js/main.js
@@ -1,1 +1,11 @@
+/*
+Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+*/
 
+
+function check() {
+    console.log()
+}
