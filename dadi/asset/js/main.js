f179685833566f8611152f486c6676048535dda4
Federico Airoldi <airoldibusiness@gmail.com>
2023-03-27 21:24:45 +0200

    dadi - inserisco la funzione al click lancia

---
+++ dadi/asset/js/main.js
@@ -0,0 +1,17 @@
+
+
+
+// Array per i numeri del primo dado
+let dado1 = [1, 2, 3, 4, 5, 6];
+
+// Array per i numeri del secondo dado
+let dado2 = [1, 2, 3, 4, 5, 6];
+
+
+
+
+
+
+function lancia() {
+    console.log('lancia')
+}
